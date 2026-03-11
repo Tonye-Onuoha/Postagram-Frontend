d14f55f4d9d1e4cb25106763bc0fa43a11139263
Tonye Hugo Onuoha <tonyeonuoha@gmail.com>
2026-03-11 01:28:34 +0100

    Added CSS loaders

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,13 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-        },
+        use: "babel-loader",
       },
     ],
   },
