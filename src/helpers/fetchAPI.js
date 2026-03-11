82b3ff8cbfeda418479940875d7bd297ab8ecea2
Tonye Hugo Onuoha <tonyeonuoha@gmail.com>
2026-03-11 05:00:40 +0100

    Updated helper module

--- src/helpers/fetchAPI.js
+++ src/helpers/fetchAPI.js
@@ -1,177 +1,211 @@
-import { getUser, getAccessToken, getRefreshToken } from "../hooks/user_actions";
+import {
+  getUser,
+  getAccessToken,
+  getRefreshToken,
+} from "../hooks/user_actions";
 
-const baseURL = process.env.NODE_ENV === "development" ? "http://localhost:8000/" : process.env.REACT_APP_API_URL;
+const baseURL =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:8000/"
+    : "https://postagram-backend-web.onrender.com";
 
 function failedRequest() {
-    throw new Error("Could not retry request!");
+  throw new Error("Could not retry request!");
 }
 
 export function setLocalStorage(access, refresh, user = null) {
-    if (user) {
-        localStorage.setItem("auth", JSON.stringify({ access, refresh, user }));
-    } else {
-        localStorage.setItem("auth", JSON.stringify({ access, refresh }));
-    }
+  if (user) {
+    localStorage.setItem("auth", JSON.stringify({ access, refresh, user }));
+  } else {
+    localStorage.setItem("auth", JSON.stringify({ access, refresh }));
+  }
 }
 
 export async function fetchData(url) {
-    const access = getAccessToken();
-    const response = await fetch(`${baseURL}${url}`, { headers: { Authorization: `Bearer ${access}` } });
-    if (response.status === 200) {
-        return await response.json();
-    } else if (response.status === 401) {
-        return await refreshAuth(url);
-    }
+  const access = getAccessToken();
+  const response = await fetch(`${baseURL}${url}`, {
+    headers: { Authorization: `Bearer ${access}` },
+  });
+  if (response.status === 200) {
+    return await response.json();
+  } else if (response.status === 401) {
+    return await refreshAuth(url);
+  }
 }
 
-export async function postData(url, body, register = false, login = false, logout = false) {
-    const data = JSON.stringify(body);
-    if (register || login) {
-        const response = await fetch(`${baseURL}${url}`, {
-            method: "POST",
-            headers: { Accept: "application/json", "Content-Type": "application/json" },
-            body: data
-        });
+export async function postData(
+  url,
+  body,
+  register = false,
+  login = false,
+  logout = false,
+) {
+  const data = JSON.stringify(body);
+  if (register || login) {
+    const response = await fetch(`${baseURL}${url}`, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: data,
+    });
 
-        return await response.json();
-    } else {
-        const access = getAccessToken();
-        const response = await fetch(`${baseURL}${url}`, {
-            method: "POST",
-            headers: {
-                Authorization: `Bearer ${access}`,
-                Accept: "application/json",
-                "Content-Type": "application/json"
-            },
-            body: data
-        });
-        if (logout) return response; // return immediately if logout request.
-        if (response.status === 200) {
-            return await response.json();
-        } else if (response.status === 401) {
-            return await refreshAuth(url, "POST", data);
-        }
+    return await response.json();
+  } else {
+    const access = getAccessToken();
+    const response = await fetch(`${baseURL}${url}`, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${access}`,
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: data,
+    });
+    if (logout) return response; // return immediately if logout request.
+    if (response.status === 200) {
+      return await response.json();
+    } else if (response.status === 401) {
+      return await refreshAuth(url, "POST", data);
     }
+  }
 }
 
 export async function updateData(url, body) {
-    const data = JSON.stringify(body);
-    const access = getAccessToken();
-    const response = await fetch(`${baseURL}${url}`, {
-        method: "PUT",
-        headers: { Authorization: `Bearer ${access}`, Accept: "application/json", "Content-Type": "application/json" },
-        body: data
-    });
-    if (response.status === 200) {
-        return await response.json();
-    } else if (response.status === 401) {
-        return await refreshAuth(url, "PUT", data);
-    }
+  const data = JSON.stringify(body);
+  const access = getAccessToken();
+  const response = await fetch(`${baseURL}${url}`, {
+    method: "PUT",
+    headers: {
+      Authorization: `Bearer ${access}`,
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: data,
+  });
+  if (response.status === 200) {
+    return await response.json();
+  } else if (response.status === 401) {
+    return await refreshAuth(url, "PUT", data);
+  }
 }
 
 export async function patchData(url, body) {
-    const access = getAccessToken();
-    const response = await fetch(`${baseURL}${url}`, {
-        method: "PATCH",
-        headers: {
-            Authorization: `Bearer ${access}`,
-            Accept: "application/json"
-        },
-        body: body
-    });
-    if (response.status === 200) {
-        return await response.json();
-    } else if (response.status === 401) {
-        return await refreshAuth(url, "PATCH", body);
-    }
+  const access = getAccessToken();
+  const response = await fetch(`${baseURL}${url}`, {
+    method: "PATCH",
+    headers: {
+      Authorization: `Bearer ${access}`,
+      Accept: "application/json",
+    },
+    body: body,
+  });
+  if (response.status === 200) {
+    return await response.json();
+  } else if (response.status === 401) {
+    return await refreshAuth(url, "PATCH", body);
+  }
 }
 
 export async function deleteData(url) {
-    const access = getAccessToken();
-    const response = await fetch(`${baseURL}${url}`, {
-        method: "DELETE",
-        headers: { Authorization: `Bearer ${access}`, Accept: "application/json", "Content-Type": "application/json" }
-    });
-    if (response.status === 204) {
-        return;
-    } else if (response.status === 401) {
-        return await refreshAuth(url, "DELETE");
-    }
+  const access = getAccessToken();
+  const response = await fetch(`${baseURL}${url}`, {
+    method: "DELETE",
+    headers: {
+      Authorization: `Bearer ${access}`,
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+  });
+  if (response.status === 204) {
+    return;
+  } else if (response.status === 401) {
+    return await refreshAuth(url, "DELETE");
+  }
 }
 
-async function refreshAuth(failedRequestURL, requestMethod = "GET", data = null) {
-    console.log("refreshing...");
-    const refresh = getRefreshToken();
-    const user = getUser();
-    const refreshData = JSON.stringify({ refresh: refresh });
-    const tokenURL = `${baseURL}/api/core/auth/token/refresh/`;
-    console.log("The refresh URL is", tokenURL);
-    const response = await fetch(tokenURL, {
-        method: "POST",
-        headers: { Authorization: `Bearer ${refresh}`, Accept: "application/json", "Content-Type": "application/json" },
-        body: refreshData
+async function refreshAuth(
+  failedRequestURL,
+  requestMethod = "GET",
+  data = null,
+) {
+  console.log("refreshing...");
+  const refresh = getRefreshToken();
+  const user = getUser();
+  const refreshData = JSON.stringify({ refresh: refresh });
+  const tokenURL = `${baseURL}/api/core/auth/token/refresh/`;
+  console.log("The refresh URL is", tokenURL);
+  const response = await fetch(tokenURL, {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${refresh}`,
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: refreshData,
+  });
+  const { access } = await response.json();
+  setLocalStorage(access, refresh, user);
+  try {
+    const response = await fetch(`${baseURL}${failedRequestURL}`, {
+      method: requestMethod,
+      headers:
+        requestMethod !== "PATCH"
+          ? {
+              Authorization: `Bearer ${access}`,
+              Accept: "application/json",
+              "Content-Type": "application/json",
+            }
+          : {
+              Authorization: `Bearer ${access}`,
+              Accept: "application/json",
+              "Content-Type": "application/json",
+            },
+      body: data,
     });
-    const { access } = await response.json();
-    setLocalStorage(access, refresh, user);
-    try {
-        const response = await fetch(`${baseURL}${failedRequestURL}`, {
-            method: requestMethod,
-            headers:
-                requestMethod !== "PATCH"
-                    ? {
-                          Authorization: `Bearer ${access}`,
-                          Accept: "application/json",
-                          "Content-Type": "application/json"
-                      }
-                    : {
-                          Authorization: `Bearer ${access}`,
-                          Accept: "application/json",
-                          "Content-Type": "application/json"
-                      },
-            body: data
-        });
 
-        switch (requestMethod) {
-            case "GET":
-                if (response.status === 200) {
-                    return await response.json();
-                } else {
-                    failedRequest();
-                }
-                break;
-            case "POST":
-                if (response.status === 201) {
-                    return await response.json();
-                } else {
-                    failedRequest();
-                }
-                break;
-            case "PUT":
-                if (response.status === 200) {
-                    return await response.json();
-                } else {
-                    failedRequest();
-                }
-                break;
-            case "PATCH":
-                if (response.status === 200) {
-                    return await response.json();
-                } else {
-                    failedRequest();
-                }
-                break;
-            case "DELETE":
-                if (response.status === 204) {
-                    return;
-                } else {
-                    failedRequest();
-                }
-                break;
-            default:
-                break;
+    switch (requestMethod) {
+      case "GET":
+        if (response.status === 200) {
+          return await response.json();
+        } else {
+          failedRequest();
         }
-    } catch (error) {
-        localStorage.removeItem("auth");
-        throw new Error(error.message);
+        break;
+      case "POST":
+        if (response.status === 201) {
+          return await response.json();
+        } else {
+          failedRequest();
+        }
+        break;
+      case "PUT":
+        if (response.status === 200) {
+          return await response.json();
+        } else {
+          failedRequest();
+        }
+        break;
+      case "PATCH":
+        if (response.status === 200) {
+          return await response.json();
+        } else {
+          failedRequest();
+        }
+        break;
+      case "DELETE":
+        if (response.status === 204) {
+          return;
+        } else {
+          failedRequest();
+        }
+        break;
+      default:
+        break;
     }
+  } catch (error) {
+    localStorage.removeItem("auth");
+    throw new Error(error.message);
+  }
 }
